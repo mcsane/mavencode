6e68b7eb0cb8c18ac8b5858e0dcd4d06e210b324
santeri <santerimulari@hotmail.com>
2023-10-03 23:39:02 +0300

    th

---
+++ src/main/java/task7_1/Curry.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS currency_converter;
+CREATE DATABASE currency_converter;
+USE currency_converter;
+
+CREATE TABLE currencies (
+    id INT NOT NULL AUTO_INCREMENT,
+    currency VARCHAR(3) NOT NULL,
+    rate DECIMAL(10, 4) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO currencies(currency, rate) VALUES
+    ('USD', 1.00),
+    ('EUR', 0.85),
+    ('GBP', 0.73),
+    ('JPY', 110.85),
+    ('AUD', 1.36),
+    ('CAD', 1.25),
+    ('CHF', 0.92),
+    ('CNY', 6.45);
+
+
+DROP USER IF EXISTS 'appuser'@'localhost';
+CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON currency_converter.* TO 'appuser'@'localhost';
+
+-- testaus
+SELECT * FROM currencies;
+SELECT * FROM currencies WHERE abbreviation = 'EUR';
+SELECT COUNT(*) FROM currencies;
+SELECT * FROM currencies ORDER BY conversion_rate DESC LIMIT 1;
